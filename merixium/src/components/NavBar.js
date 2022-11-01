b5a006ab97bb44e5dd60be3b74d3e41141f08ef9
Jesse Castillo <jesse@Jesses-MBP.attlocal.net>
2022-11-01 01:48:22 -0500

    Adding Pages

--- merixium/src/components/NavBar.js
+++ merixium/src/components/NavBar.js
@@ -1,4 +1,8 @@
+import { Button } from '@mui/material';
 import React, { useState} from 'react'
+import { Link } from 'react-router-dom';
+import './NavBar.css';
+//import { Button } from './Button';
 
 
 export default function NavBar() {
@@ -6,12 +10,25 @@ export default function NavBar() {
     const path = window.location.pathname
     const [click, setClick] = useState(false);
     const handleClick =() => setClick(!click);
+    const [button, setButton] = useState(true)
+
+    const showButton = () => {
+        if (window.innerWidth <= 960) {
+            setButton(false)
+        } else {
+            setButton(true)
+        }
+    }
 
     return (
-    <nav className="nav">
+    <nav className="navbar">
+        <div className='navbar-container'>
+            <Link to= "/'" className='navbar-logo'>
+                MERIXIUM
+            </Link>
         <div className='menu-icon' onClick={handleClick}>
          <a href="/" className="site-title">
-            Merixium
+            Merixhhhhhhhhhhhium
         </a>
         
             <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
@@ -26,7 +43,11 @@ export default function NavBar() {
             <li>
                 <a href ="gratitude">Gratitude</a>
             </li>
+            <li>
+                {button && <Button buttonStyle='btn--outline'>EXPLORE THE WONDER</Button>}
+            </li>
          </ul>
+         </div>
     </nav>
     )
 }
