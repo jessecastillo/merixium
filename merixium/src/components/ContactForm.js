152985bd007bef276d058abd6e4f52376dc81535
Jesse Castillo <jesse@Jesses-MBP.attlocal.net>
2022-11-14 01:10:28 -0600

    Adding 404 Page

--- merixium/src/components/ContactForm.js
+++ merixium/src/components/ContactForm.js
@@ -1,112 +1,40 @@
 import React from 'react';
-import { useRef } from 'react';
 import { useForm, ValidationError } from '@formspree/react';
-import emailjs from "@emailjs/browser";
-import styled from "styled-components";
 
 function ContactForm() {
   const [state, handleSubmit] = useForm("xaykajlg");
   if (state.succeeded) {
       return <p>Thanks for joining!</p>;
   }
-
-
-  
-  // npm i @emailjs/browser
-  
-  const Contact = () => {
-    const form = useRef();
-  
-    const sendEmail = (e) => {
-      e.preventDefault();
-  
-      emailjs
-        .sendForm(
-          "replace with service id",
-          "replace with template id",
-          form.current,
-          "replace with user id"
-        )
-        .then(
-          (result) => {
-            console.log(result.text);
-            console.log("message sent");
-          },
-          (error) => {
-            console.log(error.text);
-          }
-        );
-    };
-  
-    return (
-      <StyledContactForm>
-        <form ref={form} onSubmit={sendEmail}>
-          <label>Name</label>
-          <input type="text" name="user_name" />
-          <label>Email</label>
-          <input type="email" name="user_email" />
-          <label>Message</label>
-          <textarea name="message" />
-          <input type="submit" value="Send" />
-        </form>
-      </StyledContactForm>
-    );
-  };
-  
-
-  
-  // Styles
-  const StyledContactForm = styled.div`
-    width: 400px;
-  
-    form {
-      display: flex;
-      align-items: flex-start;
-      flex-direction: column;
-      width: 100%;
-      font-size: 16px;
-  
-      input {
-        width: 100%;
-        height: 35px;
-        padding: 7px;
-        outline: none;
-        border-radius: 5px;
-        border: 1px solid rgb(220, 220, 220);
-  
-        &:focus {
-          border: 2px solid rgba(0, 206, 158, 1);
-        }
-      }
-  
-      textarea {
-        max-width: 100%;
-        min-width: 100%;
-        width: 100%;
-        max-height: 100px;
-        min-height: 100px;
-        padding: 7px;
-        outline: none;
-        border-radius: 5px;
-        border: 1px solid rgb(220, 220, 220);
-  
-        &:focus {
-          border: 2px solid rgba(0, 206, 158, 1);
-        }
-      }
-  
-      label {
-        margin-top: 1rem;
-      }
-  
-      input[type="submit"] {
-        margin-top: 2rem;
-        cursor: pointer;
-        background: rgb(249, 105, 14);
-        color: white;
-        border: none;
-      }
-    }
-  `;
+  return (
+      <form onSubmit={handleSubmit}>
+      <label htmlFor="email">
+        Email Address
+      </label>
+      <input
+        id="email"
+        type="email" 
+        name="email"
+      />
+      <ValidationError 
+        prefix="Email" 
+        field="email"
+        errors={state.errors}
+      />
+      <textarea
+        id="message"
+        name="message"
+      />
+      <ValidationError 
+        prefix="Message" 
+        field="message"
+        errors={state.errors}
+      />
+      <button type="submit" disabled={state.submitting}>
+        Submit
+      </button>
+    </form>
+  );
 }
-export default ContactForm
+
+export default ContactForm;
