9ae0ac9c6615669fe0ea21a9ebcfacf6e5da53b2
Jesse Castillo <jesse@Jesses-MBP.attlocal.net>
2022-11-14 00:06:18 -0600

    Adding 404 Page

--- merixium/src/components/PageNotFound.js
+++ merixium/src/components/PageNotFound.js
@@ -8,7 +8,8 @@ function PageNotFound() {
         <div className='not-found'>
             <h1>404</h1>
             <h2>Page Not Found</h2>
-            <Link to='/'>Back to the home page...</Link>
+            <h3>Looks like that page has yet to be created or the page URL was typed incorrectly. But here's a cool picture for your troubles!</h3>
+            <Link to='/' className='link-style'>Back to the home page...</Link>
         </div>
     )
 }
