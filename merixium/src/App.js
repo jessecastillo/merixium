bb333c8524a64d24b31d20b34f1ff086b1d58f00
Jesse Castillo <jesse@Jesses-MBP.attlocal.net>
2022-11-29 10:48:46 -0600

    Revising 404

--- merixium/src/App.js
+++ merixium/src/App.js
@@ -99,7 +99,7 @@ function App() {
           <Route path = '/about' element = {<About/>} />
           <Route path = '/contact' element = {<Contact/>} />
           <Route path = '/gratitude' element = {<Gratitude />} />
-          <Route path = '*' element = {<PageNotFound /> } />
+          <Route path = '/*' element = {<PageNotFound /> } />
         </Routes>
 
         <Footer />
