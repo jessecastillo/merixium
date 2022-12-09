44f05624b563292d232f968e3f1f02ddcd5966a9
Jesse Castillo <jesse@Jesses-MacBook-Pro.local>
2022-12-08 18:40:42 -0600

    Revised About

--- merixium/src/components/AboutSection.js
+++ merixium/src/components/AboutSection.js
@@ -8,6 +8,8 @@ function AboutSection() {
             <h1>Just a little bit about who I am:</h1>
                 <h2 className='amh2'>Hello! I have a lot of titles, but I go by DJ Merixium when I am energizing the crowd!</h2>
                 <h3 className='amh4'>The rest of the time, my name is Jesse Castillo and I am beyond thrilled to use my skills to complement your event!!</h3>
+                <h3 className='amh41'>I am a top-40 open-format DJ with heavy influences from EDM, funk, R&B, and Disco. </h3>
+                <h3 className='amh41'>When in the right setting I love playing hard techno, trance, progressive trance, and industrial techno!</h3>
                 <h3 className='amh4'>I will work with you to make sure every detail is perfect and your vision is achieved.</h3>
                 <h3 className='amh3'>I created this website from scratch using React.js and have loved every second put into it from design to implementation!</h3>
         </div>
